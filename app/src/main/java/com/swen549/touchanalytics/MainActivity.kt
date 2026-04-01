126ede5581e0401df81b84b1a5f3cda6c4e22564
Zixiong Li <bl9446@g.rit.edu>
2026-04-01 13:47:22 -0400

    Login Screen and ViewModel skeleton

--- app/src/main/java/com/swen549/touchanalytics/MainActivity.kt
+++ app/src/main/java/com/swen549/touchanalytics/MainActivity.kt
@@ -4,15 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.google.firebase.Firebase
-import com.google.firebase.database.database
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.swen549.touchanalytics.ui.LoginScreen
 import com.swen549.touchanalytics.ui.theme.TouchAnalyticsTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,35 +18,21 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TouchAnalyticsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                    // Write a message to the database
-                    val database = Firebase.database
-                    val myRef = database.getReference("message")
-
-                    myRef.setValue("Hello, World!")
-
-                }
+                TouchAnalyticsApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun TouchAnalyticsApp() {
+    val navController = rememberNavController()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TouchAnalyticsTheme {
-        Greeting("Android")
+    NavHost(navController = navController, startDestination = "login") {
+        composable("login") {
+            LoginScreen(onLoginSuccess = { userId ->
+                navController.navigate("")
+            })
+        }
     }
 }
