50df7fd5fea7d7242174fd2f057c9300ffbb7cfd
Kelvin0K <havoc2830@gmail.com>
2026-04-08 01:03:06 -0400

    no longer create user directory for new user

--- app/src/main/java/com/swen549/touchanalytics/data/FirebaseClient.kt
+++ app/src/main/java/com/swen549/touchanalytics/data/FirebaseClient.kt
@@ -6,7 +6,7 @@ import com.google.firebase.database.FirebaseDatabase
 class FirebaseClient {
     private val database: FirebaseDatabase by lazy { FirebaseDatabase.getInstance() }
     
-    val usersRef: DatabaseReference by lazy { database.getReference("users") }
+    val usersRef: DatabaseReference by lazy { database.reference }
     val messagesRef: DatabaseReference by lazy { database.getReference("messages") }
     val featuresRef: DatabaseReference by lazy { database.reference }
 }
